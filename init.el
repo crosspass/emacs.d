eafcaa1337f75fd9db799d356a09264066970325
iiithking <wuyingchao1@gmail.com>
2019-01-18 19:58:42 +0800

    MacEmacs config

--- init.el
+++ init.el
@@ -50,9 +50,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+  '(custom-safe-themes
+   (quote
+    ("3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "2540689fd0bc5d74c4682764ff6c94057ba8061a98be5dd21116bf7bf301acfb" "2df493c5c7f329eef362290abdcd42a45abad98ffe33f639ecc55af084224e8b" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "73c69e346ec1cb3d1508c2447f6518a6e582851792a8c0e57a22d6b9948071b4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
  '(package-selected-packages
    (quote
-    (toml-mode go-eldoc golint vue-html-mode rspec-mode bundler goto-last-change ivy-yasnippet markdown-mode yaml-mode minitest undohist auto-complete magit flylisp ag smartparens web-mode ## flycheck projectile-rails projectile)))
+    (sublime-themes afternoon-theme solarized-theme toml-mode go-eldoc golint vue-html-mode rspec-mode bundler goto-last-change ivy-yasnippet markdown-mode yaml-mode minitest undohist auto-complete magit flylisp ag smartparens web-mode ## flycheck projectile-rails projectile)))
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -297,6 +302,28 @@ smartparens-global-mode
 (require 'auto-complete-config)
 ;;(add-hook 'go-mode-hook 'go-eldoc-setup)
 (ac-config-default)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+ (when (eq system-type 'darwin)
 
+   ;; default Latin font (e.g. Consolas)
+   (set-face-attribute 'default nil :family "Monaco")
+
+   ;; default font size (point * 10)
+   ;;
+   ;; WARNING!  Depending on the default font,
+   ;; if the size is not supported very well, the frame will be clipped
+   ;; so that the beginning of the buffer may not be visible correctly. 
+   (set-face-attribute 'default nil :height 150)
+
+   ;; use specific font for Korean charset.
+   ;; if you want to use different font size for specific charset,
+   ;; add :size POINT-SIZE in the font-spec.
+   (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
+
+   ;; you may want to add different for other charset in this way.
+   )
+(load-theme 'afternoon)
 (provide 'init)
 ;;;
